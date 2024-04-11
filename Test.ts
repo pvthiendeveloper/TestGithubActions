69681920fd17a0725afa5f81c1ab9189d9fb2d83
Thien Pham <thien.pham@ncs.com.sg>
2024-04-11 16:08:34 +0700

    test

---
+++ Test.ts
@@ -0,0 +1,16 @@
+export class Test {
+    public static test(): void {
+        console.log('Hello, world!');
+        console.log('Hello, world!');
+        console.log('Hello, world!');
+        console.log('Hello, world!');
+        console.log('Hello, world!');
+        console.log('Hello, world!');
+        console.log('Hello, world!');
+        console.log('Hello, world!');
+        console.log('Hello, world!');
+        console.log('Hello, world!');
+        console.log('Hello, world!');
+        console.log('Hello, world!');
+    }
+}
